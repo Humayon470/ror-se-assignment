ec8924b184bec4e42c02a129aa2928011133c548
Humayon 470 <mhumayon856@gmail.com>
2024-04-18 21:03:06 +0500

    Refactored the views of blog and employees

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,12 @@
 module BlogsHelper
+  def display_errors_for(object)
+    return unless object.errors.any?
+
+    content_tag(:div, class: 'text-danger') do
+      concat content_tag(:h2, pluralize(object.errors.count, "error") + " prohibited this #{object.class.model_name.human.downcase} from being saved:")
+      object.errors.full_messages.each do |message|
+        concat content_tag(:h5, message)
+      end
+    end
+  end
 end
